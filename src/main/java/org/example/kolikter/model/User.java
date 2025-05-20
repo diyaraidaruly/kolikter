1e12aa099087b4a7846eaf5b2bbc92ef9042e9b3
Aidaruly Diyar <aidaruly2005@gmail.com>
2025-05-20 23:00:11 +0500

    for Asyl

--- src/main/java/org/example/kolikter/model/User.java
+++ src/main/java/org/example/kolikter/model/User.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class User {
     private String full_name;
     private int age;
+    int a;
     private String city;
     private String phone_num;
     private String login;
